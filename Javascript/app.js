e3c7f42c33f881d6aec55ef95e756f6baec0bcb7
Htein Lynn Aung <hteinlynnaung.1111@gmail.com>
2026-03-21 22:32:51 +0700

    Admin-Dashboard

---
+++ Javascript/app.js
@@ -0,0 +1,3 @@
+console.log("Hello, VS Code!");
+let a = 5 , b = 3;
+console.log("Sum:", a + b);
---
+++ Javascript/math.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+    return a + b
+}
+
+console.log("Node Modules")
+console.log(add(1, 2))
+console.log(add(5, 7))
+console.log(add(-3, 3))
+console.log(add(0, 0))
+console.log(add(2.5, 4.5))
